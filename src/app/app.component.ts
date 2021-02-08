de93858007adcad79105884d8c0d566441632783
Miranda <jesusmiranda@medsi.com.mx>
2021-02-08 11:18:59 -0600

    se inserta archivo menu-json y se implementa primer estilo del sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { menu_json } from './menu-json/menu-json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Sidebar';
+
+  lstMenus = {};
+
+
+  constructor(){
+    console.log("Constructor");
+    this.lstMenus = menu_json;
+    console.log(this.lstMenus);
+  }
+
+  ngOnInit() {
+   console.log("ONIT");
+  }
+
+  
 }
---
+++ src/app/menu-json/menu-json.ts
@@ -0,0 +1,36 @@
+export const menu_json = [
+  {
+    id: "menu_1",
+    icon_1: "fa fa-address-book",
+    title_1: "Menu 1",
+    sub_menu_1: {
+      icon: "",
+      title: "sub menu 1",
+      sub_sub_menu_1: {
+        icon: "",
+        title: "sub sub menu 1",
+        sub_sub_sub_menu_1: {
+          icon: "",
+          title: "sub sub sub menu 1",
+        }
+      }
+    }
+  },
+  {
+    id: "menu_2",
+    icon_1: "fa fa-car",
+    title_1: "Menu 2",
+    sub_menu_1: {
+      icon: "",
+      title: "sub menu 1",
+      sub_sub_menu_1: {
+        icon: "",
+        title: "sub sub menu 1",
+        sub_sub_sub_menu_1: {
+          icon: "",
+          title: "sub sub sub menu 1",
+        }
+      }
+    }
+  }
+]
