a23a1766c0e81996727c152aeaab302e6bdab371
Flor Mariela <flormariela_15@hotmail.com>
2021-02-11 15:05:21 -0700

    se agrego un nuevo submenu en JSON y nuevo codigo CSS

--- src/app/menu-json/menu-json.ts
+++ src/app/menu-json/menu-json.ts
@@ -7,7 +7,12 @@ export const menu_json = [
       {
         id: "sub_menu_1",
         icon: "fa fa-address-book", 
-        title: "sub menu 1"
+        title: "sub menu 1",
+        sub_sub_menu : [
+          {title: "sub_menu_1_1"},
+          {title: "sub_menu_1_2"},
+          {title: "sub_menu_1_3"}
+        ]
       },
       {
         id: "sub_menu_2",
@@ -19,36 +24,102 @@ export const menu_json = [
         icon: "fa fa-address-book", 
         title: "sub menu 2"
       }
-    ]
+    ],
   },
+
+
   {
     id: "menu_2",
     icon: "fa fa-car",
     title: "Menu 2",
     sub_menu: [
-      { title: "sub menu 1" },
-      { title: "sub menu 2" },
-      { title: "sub menu 3" }
-    ]
+      { 
+        id: "sub_menu_2",
+        icon: "fa fa-car",
+        title: "sub menu 1" },
+      { id: "sub_menu_2",
+        icon: "fa fa-car",
+        title: "sub menu 2" },
+      { id: "sub_menu_2",
+        icon: "fa fa-car",
+        title: "sub menu 3" }
+    ],
   },
+
   {
     id: "menu_3",
     icon: "fa fa-magic",
     title: "Menu 3",
     sub_menu: [
-      { title: "sub menu 1" },
-      { title: "sub menu 2" },
-      { title: "sub menu 3" }
-    ]
+      { 
+        id: "menu_3",
+        icon: "fa fa-magic",
+        title: "sub menu 1" },
+      { 
+        id: "menu_3",
+        icon: "fa fa-magic",
+        title: "sub menu 2" },
+      { 
+        id: "menu_3",
+        icon: "fa fa-magic",
+        title: "sub menu 3" }
+    ],
+          sub_sub_menu : [
+          {title: "sub menu 1 1"},
+          {title: "sub menu 1 2"},
+          {title: "sub menu 1 3"}
+          ]
   },
+
   {
     id: "menu_4",
     icon: "fa fa-pencil",
     title: "Menu 4",
     sub_menu: [
-      { title: "sub menu 1" },
-      { title: "sub menu 2" },
-      { title: "sub menu 3" }
+      { 
+        id: "menu_4",
+        icon: "fa fa-pencil",
+        title: "sub menu 1" },
+      { 
+        id: "menu_4",
+        icon: "fa fa-pencil",
+        title: "sub menu 2" },
+      { 
+        id: "menu_4",
+        icon: "fa fa-pencil",
+        title: "sub menu 3" }
+    ],
+        sub_sub_menu : [
+        {title: "sub_menu_1_1"},
+        {title: "sub_menu_1_2"},
+        {title: "sub_menu_1_3"}
+        ]
+  },
+
+  {
+    id: "menu_5",
+    icon: "fa fa-coffee",
+    title: "Menu 5",
+    sub_menu: [
+      { 
+        id: "menu_5",
+        icon: "fa fa-coffee",
+        title: "sub menu 1",
+        sub_sub_menu: [
+          {title: "sub sub menu 1"},
+          {title: "sub sub menu 2"},
+          {title: "sub sub menu 3"}
+        ]  
+      },
+      { 
+        id: "menu_5",
+        icon: "fa fa-coffee",
+        title: "sub menu 2" },
+      { 
+        id: "menu_5",
+        icon: "fa fa-coffee",
+        title: "sub menu 3" }
     ]
   }
+
 ]
