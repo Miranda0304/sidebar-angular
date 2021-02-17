0c40be939b1a072b1500527c5d519df7d0bc7cb6
Flor Mariela <flormariela_15@hotmail.com>
2021-02-17 12:01:32 -0700

    New structure until  Menu 5 and submenu to  level 6

--- src/app/menu-json/menu-json.ts
+++ src/app/menu-json/menu-json.ts
@@ -37,27 +37,71 @@ export const menu_json = [
               }
             ]
           },
-          {
-            id: "id_user_menu_03_02",
-            icon: "fa fa-commenting",
-            title: "Usuarios 2"
-          }
-        ]
-      },
       {
         id: "id_user_menu_02_02",
+        icon: "fa fa-commenting",
+        title: "Usuarios 2",
+        menu_03: [
+          {
+            id: "id_user_menu_03_01",
+            icon: "fa fa-commenting",
+            title: "Usuarios 1",
+            menu_04: [
+              {
+                id: "id_user_menu_04_01",
+                icon: "fa fa-eraser",
+                title: "Usuarios 1",
+                menu_05: [
+                  {
+                    id: "id_user_menu_05_01",
+                    icon: "fa fa-deaf",
+                    title: "Usuarios 1",
+                    menu_06: [
+                      {
+                        id: "id_user_menu_06_01",
+                        icon: "fa fa-credit-card-alt",
+                        title: "Usuarios 1",
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+        } 
+      ]
+      },
+      {
+        id: "id_user_menu_03_02",
         icon: "fa fa-crop",
         title: "Crear usuario",
         menu_03: [
           {
             id: "id_user_menu_03_03",
             icon: "fa fa-commenting",
-            title: "Ver usuarios 2"
-          },
-          {
-            id: "id_user_menu_03_04",
-            icon: "fa fa-commenting",
-            title: "Crear usuario 2"
+            title: "Ver usuarios 2",
+            menu_04: [
+            {
+              id: "id_user_menu_04_01",
+              icon: "fa fa-eraser",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu_05_01",
+                  icon: "fa fa-deaf",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu_06_01",
+                      icon: "fa fa-credit-card-alt",
+                      title: "Usuarios 1"
+                    }
+                  ]
+                }
+              ]
+            }
+           ]
           }
         ]
       }
@@ -65,100 +109,434 @@ export const menu_json = [
   },
   //###################### SECOND MENU //######################
   {
-    id: "menu_2",
-    icon: "fa fa-car",
-    title: "Expedient",
-    path: 'expedient',
-    menu_02: [
-      {
-        id: "sub_menu_2",
-        icon: "fa fa-car",
-        title: "sub menu 1"
-      },
-      {
-        id: "sub_menu_2",
-        icon: "fa fa-car",
-        title: "sub menu 2"
-      },
-      {
-        id: "sub_menu_2",
-        icon: "fa fa-car",
-        title: "sub menu 3"
-      }
-    ],
-  },
-  //###################### THIRD MENU //######################
-  {
-    id: "menu_3",
-    icon: "fa fa-magic",
-    title: "Menu 3",
-    menu_02: [
-      {
-        id: "menu_3",
-        icon: "fa fa-magic",
-        title: "sub menu 1"
-      },
-      {
-        id: "menu_3",
-        icon: "fa fa-magic",
-        title: "sub menu 2"
-      },
-      {
-        id: "menu_3",
-        icon: "fa fa-magic",
-        title: "sub menu 3"
-      }
+  id: "id_users_menu2",
+  icon: "fa fa-car",
+  title: "Home",
+  path: 'home',
+  menu_02: [
+    {
+      id: "id_user_menu2_02_01",
+      icon: "fa fa-car",
+      title: "Ver usuarios",
+      menu_03: [
+        {
+          id: "id_user_menu2_03_01",
+          icon: "fa fa-car",
+          title: "Usuarios 1",
+          menu_04: [
+            {
+              id: "id_user_menu_04_01",
+              icon: "fa fa-car",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu_05_01",
+                  icon: "fa fa-deaf",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu_06_01",
+                      icon: "fa fa-credit-card-alt",
+                      title: "Usuarios 1",
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+    {
+      id: "id_user_menu2_02_02",
+      icon: "fa fa-commenting",
+      title: "Usuarios 2",
+      menu_03: [
+        {
+          id: "id_user_menu2_03_01",
+          icon: "fa fa-commenting",
+          title: "Usuarios 1",
+          menu_04: [
+            {
+              id: "id_user_menu2_04_01",
+              icon: "fa fa-eraser",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu2_05_01",
+                  icon: "fa fa-deaf",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu2_06_01",
+                      icon: "fa fa-credit-card-alt",
+                      title: "Usuarios 1",
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+      } 
     ]
-  },
-  //###################### FOURTH MENU //######################
-  {
-    id: "menu_4",
-    icon: "fa fa-pencil",
-    title: "Menu 4",
-    menu_02: [
-      {
-        id: "menu_4",
-        icon: "fa fa-pencil",
-        title: "sub menu 1"
-      },
-      {
-        id: "menu_4",
-        icon: "fa fa-pencil",
-        title: "sub menu 2"
-      },
-      {
-        id: "menu_4",
-        icon: "fa fa-pencil",
-        title: "sub menu 3"
-      }
+    },
+    {
+      id: "id_user_menu2_03_02",
+      icon: "fa fa-crop",
+      title: "Crear usuario",
+      menu_03: [
+        {
+          id: "id_user_menu2_03_03",
+          icon: "fa fa-commenting",
+          title: "Ver usuarios 2",
+          menu_04: [
+          {
+            id: "id_user_menu2_04_01",
+            icon: "fa fa-eraser",
+            title: "Usuarios 1",
+            menu_05: [
+              {
+                id: "id_user_menu2_05_01",
+                icon: "fa fa-deaf",
+                title: "Usuarios 1",
+                menu_06: [
+                  {
+                    id: "id_user_menu2_06_01",
+                    icon: "fa fa-credit-card-alt",
+                    title: "Usuarios 1"
+                  }
+                ]
+              }
+            ]
+          }
+         ]
+        }
+      ]
+    }
+  ],
+},
+//###################### THIRD MENU //######################
+{
+  id: "id_users_menu3",
+  icon: "fa fa-address-book",
+  title: "Home",
+  path: 'home',
+  menu_02: [
+    {
+      id: "id_user_menu3_02_01",
+      icon: "fa fa-crop",
+      title: "Ver usuarios",
+      menu_03: [
+        {
+          id: "id_user_menu3_03_01",
+          icon: "fa fa-commenting",
+          title: "Usuarios 1",
+          menu_04: [
+            {
+              id: "id_user_menu3_04_01",
+              icon: "fa fa-eraser",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu3_05_01",
+                  icon: "fa fa-deaf",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu3_06_01",
+                      icon: "fa fa-credit-card-alt",
+                      title: "Usuarios 1",
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+    {
+      id: "id_user_menu3_02_02",
+      icon: "fa fa-commenting",
+      title: "Usuarios 2",
+      menu_03: [
+        {
+          id: "id_user_menu3_03_01",
+          icon: "fa fa-commenting",
+          title: "Usuarios 1",
+          menu_04: [
+            {
+              id: "id_user_menu3_04_01",
+              icon: "fa fa-eraser",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu3_05_01",
+                  icon: "fa fa-deaf",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu3_06_01",
+                      icon: "fa fa-credit-card-alt",
+                      title: "Usuarios 1",
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+      } 
     ]
-  },
-  //###################### FIVE MENU //######################
-  {
-    id: "menu_5",
-    icon: "fa fa-coffee",
-    title: "Menu 5",
-    menu_02: [
-      {
-        id: "menu_5",
-        icon: "fa fa-coffee",
-        title: "sub menu 1",
-        menu_03: [
-          { title: "sub sub menu 1" },
-          { title: "sub sub menu 2" },
-          { title: "sub sub menu 3" }
-        ]
-      },
-      {
-        id: "menu_5",
-        icon: "fa fa-coffee",
-        title: "sub menu 2"
-      },
-      {
-        id: "menu_5",
-        icon: "fa fa-coffee",
-        title: "sub menu 3"
-      }
+    },
+    {
+      id: "id_user_menu3_03_02",
+      icon: "fa fa-crop",
+      title: "Crear usuario",
+      menu_03: [
+        {
+          id: "id_user_menu3_03_03",
+          icon: "fa fa-commenting",
+          title: "Ver usuarios 2",
+          menu_04: [
+          {
+            id: "id_user_menu3_04_01",
+            icon: "fa fa-eraser",
+            title: "Usuarios 1",
+            menu_05: [
+              {
+                id: "id_user_menu3_05_01",
+                icon: "fa fa-deaf",
+                title: "Usuarios 1",
+                menu_06: [
+                  {
+                    id: "id_user_menu3_06_01",
+                    icon: "fa fa-credit-card-alt",
+                    title: "Usuarios 1"
+                  }
+                ]
+              }
+            ]
+          }
+         ]
+        }
+      ]
+    }
+  ],
+}, 
+//###################### FOURTH MENU //######################
+{
+  id: "id_users_menu4",
+  icon: "fa fa-pencil",
+  title: "Home",
+  path: 'home',
+  menu_02: [
+    {
+      id: "id_user_menu4_02_01",
+      icon: "fa fa-pencil",
+      title: "Ver usuarios",
+      menu_03: [
+        {
+          id: "id_user_menu4_03_01",
+          icon: "fa fa-pencil",
+          title: "Usuarios 1",
+          menu_04: [
+            {
+              id: "id_user_menu4_04_01",
+              icon: "fa fa-pencil",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu4_05_01",
+                  icon: "fa fa-pencil",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu4_06_01",
+                      icon: "fa fa-pencil",
+                      title: "Usuarios 1",
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+    {
+      id: "id_user_menu4_02_02",
+      icon: "fa fa-pencil",
+      title: "Usuarios 2",
+      menu_03: [
+        {
+          id: "id_user_menu4_03_01",
+          icon: "fa fa-pencil",
+          title: "Usuarios 1",
+          menu_04: [
+            {
+              id: "id_user_menu4_04_01",
+              icon: "fa fa-pencil",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu4_05_01",
+                  icon: "fa fa-pencil",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu4_06_01",
+                      icon: "fa fa-pencil",
+                      title: "Usuarios 1",
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+      } 
     ]
-  }
+    },
+    {
+      id: "id_user_menu4_03_02",
+      icon: "fa fa-pencil",
+      title: "Crear usuario",
+      menu_03: [
+        {
+          id: "id_user_menu4_03_03",
+          icon: "fa fa-commenting",
+          title: "Ver usuarios 2",
+          menu_04: [
+          {
+            id: "id_user_menu4_04_01",
+            icon: "fa fa-pencil",
+            title: "Usuarios 1",
+            menu_05: [
+              {
+                id: "id_user_menu4_05_01",
+                icon: "fa fa-pencil",
+                title: "Usuarios 1",
+                menu_06: [
+                  {
+                    id: "id_user_menu4_06_01",
+                    icon: "fa fa-credit-card-alt",
+                    title: "Usuarios 1"
+                  }
+                ]
+              }
+            ]
+          }
+         ]
+        }
+      ]
+    }
+  ],
+},  
+//###################### FIVE MENU //######################
+{
+  id: "id_users_menu5",
+  icon: "fa fa-coffee",
+  title: "Home",
+  path: 'home',
+  menu_02: [
+    {
+      id: "id_user_menu5_02_01",
+      icon: "fa fa-coffee",
+      title: "Ver usuarios",
+      menu_03: [
+        {
+          id: "id_user_menu5_03_01",
+          icon: "fa fa-coffee",
+          title: "Usuarios 1",
+          menu_04: [
+            {
+              id: "id_user_menu5_04_01",
+              icon: "fa fa-coffee",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu5_05_01",
+                  icon: "fa fa-coffee",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu5_06_01",
+                      icon: "fa fa-coffee",
+                      title: "Usuarios 1",
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+    {
+      id: "id_user_menu5_02_02",
+      icon: "fa fa-coffee",
+      title: "Usuarios 2",
+      menu_03: [
+        {
+          id: "id_user_menu5_03_01",
+          icon: "fa fa-coffee",
+          title: "Usuarios 1",
+          menu_04: [
+            {
+              id: "id_user_menu5_04_01",
+              icon: "fa fa-coffee",
+              title: "Usuarios 1",
+              menu_05: [
+                {
+                  id: "id_user_menu5_05_01",
+                  icon: "fa fa-coffee",
+                  title: "Usuarios 1",
+                  menu_06: [
+                    {
+                      id: "id_user_menu5_06_01",
+                      icon: "fa fa-coffee",
+                      title: "Usuarios 1",
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+      } 
+    ]
+    },
+    {
+      id: "id_user_menu5_03_02",
+      icon: "fa fa-coffee",
+      title: "Crear usuario",
+      menu_03: [
+        {
+          id: "id_user_menu5_03_03",
+          icon: "fa fa-commenting",
+          title: "Ver usuarios 2",
+          menu_04: [
+          {
+            id: "id_user_menu5_04_01",
+            icon: "fa fa-coffee",
+            title: "Usuarios 1",
+            menu_05: [
+              {
+                id: "id_user_menu5_05_01",
+                icon: "fa fa-coffee",
+                title: "Usuarios 1",
+                menu_06: [
+                  {
+                    id: "id_user_menu5_06_01",
+                    icon: "fa fa-credit-card-alt",
+                    title: "Usuarios 1"
+                  }
+                ]
+              }
+            ]
+          }
+         ]
+        }
+      ]
+    }
+  ],
+}    
 ]
