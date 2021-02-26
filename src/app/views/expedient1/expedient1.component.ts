b4774a0f53a605c26298a2c3d8c453cdb0b34af2
JesusMiranda <jesusmiranda0304@gmail.com>
2021-02-26 08:47:41 -0600

    read any json on dynamic form

--- src/app/views/expedient1/expedient1.component.ts
+++ src/app/views/expedient1/expedient1.component.ts
@@ -10,13 +10,34 @@ import { InfobarService } from "../../services/inforbar_visible/visible.service"
 export class Expedient1Component implements OnInit {
 
   lstForms = [];
-  ngSwitchCase: any
+
   constructor(private _serviceInfobar: InfobarService) {
     this._serviceInfobar.isVisible(false);
-    this.lstForms = form_expedient1;
+    this.lstForms = this.changeKeysList(form_expedient1)
   }
 
   ngOnInit(): void {
+  }
+  
+  renameKey = (object, key, newKey) => {
+    const clonedObj = Object.assign({}, object);
+    const targetKey = clonedObj[key];
+    delete clonedObj[key];
+    clonedObj[newKey] = targetKey;
+    return clonedObj;
+  };
+
+  changeKeysList(list_array = []) {
+    let newLst = [];
+    let keys = Object.keys(list_array[0])
+
+    list_array.forEach((element) => {
+      keys.forEach((key, idx) => {
+        element = this.renameKey(element, key, `item${idx}`)
+      });
+      newLst.push(element)
+    });
+    return newLst;
   }
 
 }
