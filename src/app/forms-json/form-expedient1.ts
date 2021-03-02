d35c7b41957ae367076d4f52c07c61e0cba94679
Flor Mariela <flormariela_15@hotmail.com>
2021-03-02 13:17:39 -0700

    change color background menu and infobar

--- src/app/forms-json/form-expedient1.ts
+++ src/app/forms-json/form-expedient1.ts
@@ -13,4 +13,6 @@ export const form_expedient1 = [
     { id: '8', size_col: 'col-md-2', have_label: true, label_title: 'Radio 2 - Grupo 1', field_type: 'radiobutton', field_id: 'input_expedient_radiobutton_2', field_class: 'form-check-input', placeholder_name: 'group1' },
 
     { id: '9', size_col: 'col-md-2', have_label: true, label_title: 'Click me', field_type: 'button', field_id: 'input_expedient_button', field_class: 'btn btn-primary', placeholder_name: '' },
+
+    { id: '1', size_col: 'col-md-6', have_label: true, label_title: 'Nombre', field_type: 'text', field_id: 'input_expedient_first_name', field_class: 'form-control', placeholder_name: 'Nombre' },
 ]
