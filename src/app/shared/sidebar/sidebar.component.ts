4d8ea6c0b09a81b03a40eb5a28a1834007388aa4
JesusMiranda <jesusmiranda0304@gmail.com>
2021-03-02 18:40:04 -0600

    refactor keys sidebar html

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -32,8 +32,8 @@ export class SidebarComponent implements OnInit {
 
   constructor(private _serviceObjects: ObjectsService) {
     this.list_menu_level_01 = menu_json.data;
-    this.list_menu_level_01 = this._serviceObjects.changeAllKeys(this.list_menu_level_01);
-   
+    //this.list_menu_level_01 = this._serviceObjects.changeAllKeys(this.list_menu_level_01);
+    this.list_menu_level_01 = this.list_menu_level_01;
   }
 
   ngOnInit(): void {
@@ -42,42 +42,42 @@ export class SidebarComponent implements OnInit {
   openSubMenu(menu_id: number, level_to_open: number) {
 
     if (level_to_open == 2) {
-      this.list_menu_level_02 = this.list_menu_level_01.filter(x => x.item0 == menu_id).map(x => x.item4)[0]
-      this.list_menu_level_02 = this._serviceObjects.changeAllKeys(this.list_menu_level_02);
+      this.list_menu_level_02 = this.list_menu_level_01.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
+      this.list_menu_level_02 = this.list_menu_level_02;
 
-      this.list_menu_level_02.length > 0 ?
+      this.list_menu_level_02 != undefined ?
         this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
 
     }
     if (level_to_open == 3) {
-      this.list_menu_level_03 = this.list_menu_level_02.filter(x => x.item0 == menu_id).map(x => x.item4)[0]
-      this.list_menu_level_03 = this._serviceObjects.changeAllKeys(this.list_menu_level_03);
+      this.list_menu_level_03 = this.list_menu_level_02.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
+      this.list_menu_level_03 = this.list_menu_level_03;
 
-      this.list_menu_level_03.length > 0 ?
+      this.list_menu_level_03 != undefined ?
         this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
 
     }
     if (level_to_open == 4) {
-      this.list_menu_level_04 = this.list_menu_level_03.filter(x => x.item0 == menu_id).map(x => x.item4)[0]
-      this.list_menu_level_04 = this._serviceObjects.changeAllKeys(this.list_menu_level_04);
+      this.list_menu_level_04 = this.list_menu_level_03.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
+      this.list_menu_level_04 = this.list_menu_level_04;
 
-      this.list_menu_level_04.length > 0 ?
+      this.list_menu_level_04 != undefined ?
         this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
 
     }
     if (level_to_open == 5) {
-      this.list_menu_level_05 = this.list_menu_level_04.filter(x => x.item0 == menu_id).map(x => x.item4)[0]
-      this.list_menu_level_05 = this._serviceObjects.changeAllKeys(this.list_menu_level_05);
+      this.list_menu_level_05 = this.list_menu_level_04.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
+      this.list_menu_level_05 = this.list_menu_level_05;
 
-      this.list_menu_level_05.length > 0 ?
+      this.list_menu_level_05 != undefined ?
         this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
 
     }
     if (level_to_open == 6) {
-      this.list_menu_level_06 = this.list_menu_level_05.filter(x => x.item0 == menu_id).map(x => x.item4)[0]
-      this.list_menu_level_06 = this._serviceObjects.changeAllKeys(this.list_menu_level_06);
+      this.list_menu_level_06 = this.list_menu_level_05.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
+      this.list_menu_level_06 = this.list_menu_level_06;
 
-      this.list_menu_level_06.length > 0 ?
+      this.list_menu_level_06 != undefined ?
         this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
 
     }
