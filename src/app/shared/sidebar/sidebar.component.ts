7d43fe887b852d0d089e7476174f46c01894bf73
JesusMiranda <jesusmiranda0304@gmail.com>
2021-03-04 12:50:09 -0600

    install boostrap 4.6, jquery, popper js and font awesome in dependencies local, fix navbar searcher position, list menus refactor in object, add isCollapse in ilstVisible, validators beta 1

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -13,33 +13,35 @@ export class SidebarComponent implements OnInit {
   //Searcher
   @Input() searcherText = "";
 
-  //Lists
-  list_menu_level_01 = [];
-  list_menu_level_02 = [];
-  list_menu_level_03 = [];
-  list_menu_level_04 = [];
-  list_menu_level_05 = [];
-  list_menu_level_06 = [];
-  list_menu_level_07 = [];
+  list_menus = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+  };
 
-  contextual_area: false;
+  contextual_area: boolean;
 
   //Visibility of level
   lstVisible = [
-    { level: 2, isVisible: false },
-    { level: 3, isVisible: false },
-    { level: 4, isVisible: false },
-    { level: 5, isVisible: false },
-    { level: 6, isVisible: false },
-    { level: 7, isVisible: false },
+    { level: 1, isVisible: false, isCollapse: false },
+    { level: 2, isVisible: false, isCollapse: false },
+    { level: 3, isVisible: false, isCollapse: false },
+    { level: 4, isVisible: false, isCollapse: false },
+    { level: 5, isVisible: false, isCollapse: false },
+    { level: 6, isVisible: false, isCollapse: false },
+    { level: 7, isVisible: false, isCollapse: false },
+    { level: 8, isVisible: false, isCollapse: false },
   ]
 
   constructor(private _serviceObjects: ObjectsService, private _serviceContextualArea: ContextualAreaService) {
-    this.list_menu_level_01 = menu_json.data;
-    //this.list_menu_level_01 = this._serviceObjects.changeAllKeys(this.list_menu_level_01);
-    this.list_menu_level_01 = this.list_menu_level_01;
-
-    this.contextual_area = this.list_menu_level_01.filter(x => x.id)[0].contextual_area
+    this.list_menus[1] = menu_json.data;
+    localStorage.getItem('CONTEXTUAL_AREA') == 'true' || localStorage.getItem('CONTEXTUAL_AREA') == '1' ? this.contextual_area = true : this.contextual_area = false;
     this._serviceContextualArea.isVisible(this.contextual_area);
   }
 
@@ -48,75 +50,26 @@ export class SidebarComponent implements OnInit {
 
   openSubMenu(menu_id: number, level_to_open: number) {
 
-    if (level_to_open == 2) {
-      this.list_menu_level_02 = this.list_menu_level_01.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
+    this.list_menus[level_to_open] = this.list_menus[level_to_open - 1].filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
 
-      //Contextual Area level 1
-      this.contextual_area = this.list_menu_level_01.filter(x => x.id == menu_id)[0].contextual_area
-      this._serviceContextualArea.isVisible(this.contextual_area);
+    this.list_menus[level_to_open] != undefined ?
+      this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
 
-      this.list_menu_level_02 != undefined ?
-        this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
+    let collapse = this.list_menus[level_to_open - 1].filter(x => x.id == menu_id)[0].collapse;
+    this.lstVisible.filter(x => x.level == level_to_open - 1).map(x => x.isCollapse = collapse)
 
-    }
-    if (level_to_open == 3) {
-      this.list_menu_level_03 = this.list_menu_level_02.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
+    this.saveContextualArea(menu_id, this.list_menus[level_to_open - 1]);
 
-      //Contextual Area level 2
-      this.contextual_area = this.list_menu_level_02.filter(x => x.id == menu_id)[0].contextual_area
-      this._serviceContextualArea.isVisible(this.contextual_area);
-
-      this.list_menu_level_03 != undefined ?
-        this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
-
-    }
-    if (level_to_open == 4) {
-      this.list_menu_level_04 = this.list_menu_level_03.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
-
-      //Contextual Area level 3
-      this.contextual_area = this.list_menu_level_03.filter(x => x.id == menu_id)[0].contextual_area
-      this._serviceContextualArea.isVisible(this.contextual_area);
-
-      this.list_menu_level_04 != undefined ?
-        this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
-
-    }
-    if (level_to_open == 5) {
-      this.list_menu_level_05 = this.list_menu_level_04.filter(x => x.id == menu_id).map(x => x.sub_menu)[0];
-
-      //Contextual Area level 4
-      this.contextual_area = this.list_menu_level_04.filter(x => x.id == menu_id)[0].contextual_area
-      this._serviceContextualArea.isVisible(this.contextual_area);
-
-      this.list_menu_level_05 != undefined ?
-        this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
-
-    }
-    if (level_to_open == 6) {
-      this.list_menu_level_06 = this.list_menu_level_05.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
-
-      //Contextual Area level 5
-      this.contextual_area = this.list_menu_level_05.filter(x => x.id == menu_id)[0].contextual_area
-      this._serviceContextualArea.isVisible(this.contextual_area);
-
-      this.list_menu_level_06 != undefined ?
-        this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
-
-    }
-    if (level_to_open == 7) {
-      this.list_menu_level_07 = this.list_menu_level_06.filter(x => x.id == menu_id).map(x => x.sub_menu)[0]
-
-      //Contextual Area level 6
-      this.contextual_area = this.list_menu_level_06.filter(x => x.id == menu_id)[0].contextual_area
-      this._serviceContextualArea.isVisible(this.contextual_area);
-
-      this.list_menu_level_07 != undefined ?
-        this.lstVisible.map(x => x.level <= level_to_open ? x.isVisible = true : x.isVisible = false) : this.lstVisible.map(x => x.level < level_to_open ? x.isVisible = true : x.isVisible = false);
-
-    }
     setTimeout(() => {
       window.scroll(10000, 0);
     }, 1);
+  }
+
+  saveContextualArea(menu_id: number, lst = []) {
+    //Contextual Area
+    this.contextual_area = lst.filter(x => x.id == menu_id)[0].contextual_area
+    this._serviceContextualArea.isVisible(this.contextual_area);
+    localStorage.setItem("CONTEXTUAL_AREA", this.contextual_area.toString());
   }
 
 }
